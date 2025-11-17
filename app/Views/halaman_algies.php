010758b59ab90b4d61b91794a3c6106d5da440e7
Repan666 <alejandrorepan@gmail.com>
2025-11-17 08:23:06 +0700

    Menambahkan Views New

---
+++ app/Views/halaman_algies.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to Branch Algies</h1>
+    <p>
+        GitHub adalah platform berbasis web yang 
+        memungkinkan pengembang untuk menyimpan, berbagi, dan berkolaborasi 
+        dalam proyek kode menggunakan sistem kontrol versi Git. Ini berfungsi sebagai 
+        tempat penyimpanan daring untuk repositori Git dan menyediakan alat 
+        untuk manajemen proyek, pelacakan perubahan kode, dan kolaborasi tim 
+        secara efisien.
+        </p>
+</body>
+</html>
